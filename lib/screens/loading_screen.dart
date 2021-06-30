80ab2ed460d077e042a6b8c29a8ad99332377cd9
Nick Gutierrez <cimbyote@gmail.com>
2021-06-30 15:50:50 -0500

    restarted, using filler location data
    the geolocation flutter plug in wasn't working, so I just used filler data

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -6,6 +7,15 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+
+  void getLocation() async{
+    //Geolocator just isn't working, so just made some stuff up for Chicago
+    //Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    DateTime tempTime = new DateTime(1,1,1,1,1,1,1);
+    Position position = new Position(longitude: 41.8781, latitude: -87.6298, timestamp: tempTime, accuracy: 0.0, altitude: 0.0, heading: 0.0, speed: 0.0, speedAccuracy: 0.00);
+    print(position);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,6 +23,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: RaisedButton(
           onPressed: () {
             //Get the current location
+            getLocation();
           },
           child: Text('Get Location'),
         ),
